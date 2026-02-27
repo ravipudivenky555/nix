972451eb82cca8ff681846cab4ef9b5ad83a3bf2
Venkatesh Ravipudi <venkateshravipudi555@gmail.com>
2026-02-27 16:18:07 +0530

    Start NM Applet at startup

--- programs/niri/niri.nix
+++ programs/niri/niri.nix
@@ -8,6 +8,7 @@
     enable = true;
     wlr.enable = true;
     xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
   };
   environment.etc."niri/config.kdl".source = ./config.kdl;
   environment.systemPackages = with pkgs; [
