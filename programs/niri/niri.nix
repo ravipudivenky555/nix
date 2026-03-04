4a34df15809f042886d7f856e5debd83e7f8560a
Venkatesh Ravipudi <venkateshravipudi555@gmail.com>
2026-03-04 12:36:58 +0530

    Install and Enable power profiles daemon

--- programs/niri/niri.nix
+++ programs/niri/niri.nix
@@ -4,6 +4,7 @@
   programs.nm-applet.enable = true;
   programs.waybar.enable = true;
   programs.xwayland.enable = true;
+  services.power-profiles-daemon.enable = true;
   xdg.portal = {
     enable = true;
     wlr.enable = true;
