ca821f1474dcebb0039250d648678bd3fab6c4eb
Venkatesh Ravipudi <venkateshravipudi555@gmail.com>
2026-03-20 17:25:06 +0530

    Add diagnostics to nvim

---
+++ configs/nvim/lua/plugins/diagnostics.lua
@@ -0,0 +1,14 @@
+return {
+	"folke/trouble.nvim",
+	opts = {},
+	cmd = "Trouble",
+	keys = { {
+		"<leader>dt",
+		"<cmd>Trouble diagnostics toggle<CR>",
+		desc = "Trouble diagnostics toggle"
+	}, {
+		"<leader>ds",
+		"<cmd>Trouble symbols toggle<CR>",
+		desc = "Trouble symbols toggle"
+	}, },
+}
