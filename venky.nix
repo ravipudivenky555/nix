5ef0a1a0e229314a40316d00459818b600361f71
Venkatesh Ravipudi <venkateshravipudi555@gmail.com>
2026-04-25 12:07:05 +0530

    Enable dunst

--- venky.nix
+++ venky.nix
@@ -5,6 +5,8 @@ in
 {
   home.stateVersion = "26.05";
 
+  services.dunst.enable = true;
+
   xdg.configFile."nvim".source = mkSymlink ./configs/nvim;
   xdg.configFile."niri".source = mkSymlink ./configs/niri;
   xdg.configFile."kitty".source = mkSymlink ./configs/kitty;
