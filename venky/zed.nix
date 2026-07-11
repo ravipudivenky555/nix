5c7374c3a3bd50349bf90a02951c48f4790b4f55
Venkatesh Ravipudi <venkateshravipudi555@gmail.com>
2026-07-11 13:10:23 +0530

    missing files

---
+++ venky/zed.nix
@@ -0,0 +1,62 @@
+{ ... }: {
+  programs.zed-editor = {
+    enable = true;
+    extensions = [
+      "dart"
+      "flutter-snippets"
+    ];
+    userSettings = {
+      assistant.enabled = false;
+      auto_signature_help = true;
+      calls = {
+        mute_on_join = false;
+        share_on_join = false;
+      };
+      collaboration_panel = {
+        button = false;
+      };
+      colorize_brackets = true;
+      diagnostics = {
+        button = true;
+        inline = {
+          enabled = true;
+          padding = 5;
+        };
+      };
+      disable_ai = true;
+      editor_tabs = {
+        file_icon = true;
+        git_status = true;
+        show_diagnostics = "errors";
+      };
+      hour_format = "hour12";
+      inlay_hints = {
+        enabled = true;
+      };
+      lsp = {
+        "*" = {
+          binary.path_lookup = true;
+        };
+      };
+      languages = {
+        "*" = {
+          formatter = "language_server";
+          format_on_save = "on";
+          tab_size = 2;
+        };
+      };
+      relative_line_numbers = "enabled";
+      semantic_tokens = "combined";
+      telemetry = {
+        diagnostics = true;
+        metrics = false;
+      };
+      terminal = {
+        alternate_scroll = "on";
+        cursor_shape = "bar";
+      };
+      use_smartcase_search = true;
+      vim_mode = true;
+    };
+  };
+}
