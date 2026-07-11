04290fcaf70f0a056ebd1c2a6c6c2df2ac76fd6d
Venkatesh Ravipudi <venkateshravipudi555@gmail.com>
2026-07-11 16:32:24 +0530

    Keybinds for zed vim mode

--- venky/zed.nix
+++ venky/zed.nix
@@ -7,6 +7,7 @@
     ];
     userSettings = {
       assistant.enabled = false;
+      auto_signature_help = false;
       calls = {
         mute_on_join = false;
         share_on_join = false;
@@ -57,5 +58,26 @@
       use_smartcase_search = true;
       vim_mode = true;
     };
+    userKeymaps = [
+      {
+        "context" = "Editor && vim_mode == normal";
+        "bindings" = {
+          "q q" = "pane::CloseActiveItem";
+          "q w" = "workspace::Save";
+          "q e" = [
+            "action::Sequence"
+            [
+              "workspace::Save"
+              "pane::CloseActiveItem"
+            ]
+          ];
+
+          "s v" = "pane::SplitRight";
+          "s h" = "pane::SplitDown";
+
+          ";" = "command_palette::Toggle";
+        };
+      }
+    ];
   };
 }
