6ee429b37cd33d0fda5adf334825a963f884f08a
Venkatesh Ravipudi <venkateshravipudi555@gmail.com>
2026-07-21 09:33:32 +0530

    Remove zed

--- venky.nix
+++ venky.nix
@@ -5,8 +5,6 @@ in
 {
   home.stateVersion = "26.11";
 
-  imports = [ ./venky/zed.nix ];
-
   xdg.configFile."hypr".source = mkSymlink ./configs/hypr;
   xdg.configFile."nvim".source = mkSymlink ./configs/nvim;
   xdg.configFile."kitty".source = mkSymlink ./configs/kitty;
